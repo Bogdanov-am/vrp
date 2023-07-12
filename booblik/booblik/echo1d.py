915ed7db49f2b9e5891529da68029393f294705f
Bogdanov Anton <bogdan0v.am@yandex.ru>
2023-07-12 13:00:42 +0300

    ping

--- booblik/booblik/echo1d.py
+++ booblik/booblik/echo1d.py
@@ -4,56 +4,58 @@ from threading import Thread
 import rclpy
 from rclpy.node import Node
 from booblik_msg.msg import Echo1d
+import time
+from datetime import datetime
 
 @dataclass
 class Echo1dConfig:
     port: str
     baudrate: int
+    period: float # ms
+    speed_of_sound: float # m/s
 
 
 class Echo1dNode(Node):
     config: Echo1dConfig
 
-    def __init__(self, name='gpsimu'):
+    def __init__(self, name='echo_ping'):
         super().__init__(name)
-        self.config = Echo1dConfig('COM4', 9600)
+        self.config = Echo1dConfig(
+            '/dev/ttyUSB0', 
+            115200,
+            500,
+            1490
+        )
         self.echo1d_ = self.create_publisher(
             Echo1d,
             '/booblik/sensors/echo1d',
             10)
-        self.echo1d_
 
         Thread(target=self._readLoop, daemon=True).start()
 
     def _readLoop(self):
-        myPing = Ping1D()
-        myPing.connect_serial("/dev/ttyUSB0", 115200)
-        if myPing.initialize() is False:
+        ping = Ping1D()
+        ping.connect_serial(self.config.port, self.config.baudrate)
+        if ping.initialize() is False:
             print("Failed to initialize Ping!")
             exit(1)
+        ping.set_speed_of_sound(self.config.speed_of_sound * 1000)
 
-        print(myPing.get_device_id())
-        print(myPing.get_device_information())
-        print(myPing.get_distance())
-        print(myPing.get_distance_simple())
-        print(myPing.get_gain_setting())
-        print(myPing.get_firmware_version())
-        print(myPing.get_general_info())
-        print(myPing.get_mode_auto())
-        print(myPing.get_pcb_temperature())
-        print(myPing.get_ping_enable())
-        print(myPing.get_ping_interval())
-        print(myPing.get_profile())
-        print(myPing.get_processor_temperature())
-        print(myPing.get_speed_of_sound())
-        print(myPing.get_range())
-        print(myPing.get_transmit_duration())
-        print(myPing.get_protocol_version())
-        # if data:
-        #     print(data)
-        #     # print("Distance: %s\tConfidence: %s%%" % (data["distance"], data["confidence"]))
-        # else:
-        #     print("Failed to get distance data")
+        while True:
+            start = datetime.now()
+            data = ping.get_distance()
+            msg = Echo1d()
+            msg.distance = data["distance"]
+            msg.confidence = data["confidence"]
+            msg.scan_start = data["scan_start"]
+            msg.scan_length = data["scan_length"]
+            msg.transmit_duration = data["transmit_duration"]
+            msg.gain_setting = data["gain_setting"]
+
+            self.echo1d_.publish(msg)
+            self.get_logger().info("Distance: %s\tConfidence: %s%%" % (data["distance"], data["confidence"]))
+            time.sleep(self.config.period/1000)
+
 
 
 def main(args=None):
